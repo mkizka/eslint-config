a93bc005597ae31e5f7451addf026670cc2ffc07
mkizka <g.mkizka@gmail.com>
2024-03-31 18:35:58 +0900

    リファクタリング

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { FlatCompat } from "@eslint/eslintrc";
 import eslint from "@eslint/js";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
+import type { ConfigWithExtends } from "typescript-eslint";
 import tseslint from "typescript-eslint";
 
 const compat = new FlatCompat();
@@ -9,43 +10,57 @@ type Options = {
   alias?: Record<string, string>;
 };
 
-export const mkizka = (options?: Options): ReturnType<typeof tseslint.config> =>
-  tseslint.config(eslint.configs.recommended, {
-    files: ["**/*.ts", "**/*.tsx"],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        project: true,
+export const mkizka = (
+  options?: Options,
+): ReturnType<typeof tseslint.config> => {
+  const configs: ConfigWithExtends[] = [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+      languageOptions: {
+        parserOptions: {
+          project: true,
+        },
+      },
+      rules: {
+        "@typescript-eslint/ban-ts-comment": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/consistent-type-imports": "error",
+        "@typescript-eslint/no-floating-promises": "error",
       },
     },
-    extends: [
-      ...tseslint.configs.recommended,
-      ...compat.extends(
-        "plugin:eqeqeq-fix/recommended",
-        "plugin:@dword-design/import-alias/recommended",
-      ),
-      ...compat.plugins("unused-imports"),
-    ],
-    plugins: {
-      "@typescript-eslint": tseslint.plugin,
-      "simple-import-sort": simpleImportSort,
+    {
+      plugins: {
+        "simple-import-sort": simpleImportSort,
+      },
+      rules: {
+        "simple-import-sort/imports": "error",
+        "simple-import-sort/exports": "error",
+      },
     },
-    rules: {
-      // @typescript-eslint
-      "@typescript-eslint/ban-ts-comment": "off",
-      "@typescript-eslint/no-non-null-assertion": "off",
-      "@typescript-eslint/consistent-type-imports": "error",
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/eqeqeq": "off",
-      "@typescript-eslint/no-floating-promises": "error",
-      // simple-import-sort
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error",
-      // @dword-design/import-alias
-      "@dword-design/import-alias/prefer-alias": options?.alias
-        ? ["error", { alias: options.alias }]
-        : "off",
-      // unused-imports
-      "unused-imports/no-unused-imports": "error",
+    {
+      extends: compat.plugins("unused-imports"),
+      rules: {
+        "@typescript-eslint/no-unused-vars": "off",
+        "unused-imports/no-unused-imports": "error",
+      },
     },
-  });
+    {
+      extends: compat.extends("plugin:eqeqeq-fix/recommended"),
+      rules: {
+        "@typescript-eslint/eqeqeq": "off",
+      },
+    },
+  ];
+  if (options?.alias) {
+    configs.push({
+      extends: compat.extends("plugin:@dword-design/import-alias/recommended"),
+      rules: {
+        "@dword-design/import-alias/prefer-alias": options?.alias
+          ? ["error", { alias: options.alias }]
+          : "off",
+      },
+    });
+  }
+  return tseslint.config(...configs);
+};
