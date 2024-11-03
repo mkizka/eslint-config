3dbc088d78dc8e7055451f11b9bbc64ddbca087d
mkizka <g.mkizka@gmail.com>
2024-11-03 23:34:37 +0900

    compilerOptionsがなくてもエラーにしない

--- utils/read-alias.ts
+++ utils/read-alias.ts
@@ -3,26 +3,27 @@ import JSONC from "jsonc-parser";
 import path from "path";
 
 type TSConfig = {
-  compilerOptions: {
+  compilerOptions?: {
     paths?: Record<string, string[]>;
   };
 };
 
 const readTsconfigPaths = () => {
-  try {
-    const file = fs.readFileSync(
-      path.join(process.cwd(), "tsconfig.json"),
-      "utf-8",
-    );
-    const tsconfig = JSONC.parse(file) as TSConfig;
-    return tsconfig.compilerOptions.paths ?? {};
-  } catch {
-    // eslint-disable-next-line no-console
-    console.error(
-      "[@mkizka/eslint-config] tsconfig.jsonの読み込みに失敗しました",
-    );
+  const file = (() => {
+    try {
+      return fs.readFileSync(
+        path.join(process.cwd(), "tsconfig.json"),
+        "utf-8",
+      );
+    } catch {
+      return null;
+    }
+  })();
+  if (!file) {
     return {};
   }
+  const tsconfig = JSONC.parse(file) as TSConfig;
+  return tsconfig.compilerOptions?.paths ?? {};
 };
 
 export const readAliasFromTsconfig = () => {
