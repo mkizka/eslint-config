a4c71d8f018785f360d1626bf4d3899745ac19ba
mkizka <g.mkizka@gmail.com>
2025-03-21 18:48:55 +0900

    設定いろいろ整理

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
-  external: ["@eslint/js"],
   dts: true,
   clean: true,
 });
